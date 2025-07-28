6e00fe9b9e4fba5704196c301fd34b8ef47c84d1
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-07-28 19:01:32 +0200

    Update ContattoLavoro.cs
    scritto costruttore per il contatto lavorativo

--- MyRubricaProgettoEstate/Assets/Script/ContattoLavoro.cs
+++ MyRubricaProgettoEstate/Assets/Script/ContattoLavoro.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using System;
 
 public class ContattoLavoro : Contatto
 {
     private string nomeAzienda;
     private string emailAziendale;
+
+    public ContattoLavoro(string nome, string telefono, string nomeAzienda, string emailAziendale) : base(nome, telefono, TipoContatto.Lavoro)
+    {
+        this.nomeAzienda = nomeAzienda;
+        this.emailAziendale = emailAziendale;
+    }
 }
