d1bf640b59398fccd8e138545038b84d365e89e2
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-07-28 19:35:14 +0200

    creata classe utente e scritti i suoi attributi

---
+++ MyRubricaProgettoEstate/Assets/Script/Utente.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class Utente
+{
+    private string telefono;
+    private string password;
+    private string nomeUtente;
+    private List<Contatto> listaContatti;
+}
