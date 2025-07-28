01dc67036fa56ec0010df3270a67f0b494b422b0
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-07-28 18:18:57 +0200

    Update Contatto.cs
    scritto costruttore della classe contatto

--- MyRubricaProgettoEstate/Assets/Script/Contatto.cs
+++ MyRubricaProgettoEstate/Assets/Script/Contatto.cs
@@ -5,5 +5,12 @@ public class Contatto
     private string nome;
     private string telefono;
     private TipoContatto categoria;
-    
+
+
+    public Contatto(string nome, string telefono, TipoContatto categoria)
+    {
+        this.nome = nome;
+        this.telefono = telefono;
+        this.categoria = categoria;
+    }
 }
