df3d70440a3a55e37c56a0834a73442ba59ed554
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-07-28 18:41:50 +0200

    Update ContattoPrivato.cs
    scritto costruttore della classe del contatto privato

--- MyRubricaProgettoEstate/Assets/Script/ContattoPrivato.cs
+++ MyRubricaProgettoEstate/Assets/Script/ContattoPrivato.cs
@@ -6,5 +6,9 @@ public class ContattoPrivato : Contatto
     private string indirizzo;
     private string soprannome;
 
-
+    public ContattoPrivato(string nome, string telefono, string indirizzo, string soprannome) : base(nome, telefono, TipoContatto.Privato)
+    {
+        this.indirizzo = indirizzo;
+        this.soprannome = soprannome;
+    }
 }
