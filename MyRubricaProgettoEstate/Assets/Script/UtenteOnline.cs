3e372ac65c1a9e04fe4cf01a6fe4b54fe4c60b5c
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-07-29 18:53:27 +0200

    Update UtenteOnline.cs
    aggiunti commenti nella classe utente online

--- MyRubricaProgettoEstate/Assets/Script/UtenteOnline.cs
+++ MyRubricaProgettoEstate/Assets/Script/UtenteOnline.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public static class UtenteOnline
 {
+
+    //attributo
     private static Utente utenteLoggato;
 
+    //getter
     public static Utente GetUtenteLoggato()
     {
         return utenteLoggato;
     }
 
+    //setter
     public static void SetUtenteLoggato(Utente utente)
     {
         utenteLoggato = utente;
