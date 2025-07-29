ec1ef1d41b335777b7c0d6f873c5a926dc71c567
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-07-29 18:43:10 +0200

    Update GestoreUtenti.cs
    scritto metodo per registrare un utente

--- MyRubricaProgettoEstate/Assets/Script/GestoreUtenti.cs
+++ MyRubricaProgettoEstate/Assets/Script/GestoreUtenti.cs
@@ -3,4 +3,18 @@ using System.Collections.Generic;
 public static class GestoreUtenti
 {
     private static List<Utente> utenti = new List<Utente>();
+
+    public static bool RegistraUtente(string telefono, string password, string nomeUtente)
+    {
+        foreach (Utente utente in utenti)
+        {
+            if (utente.GetTelefono() == telefono)
+            {
+                return false;
+            }
+        }
+        Utente utenteDaAggiungere = new Utente(telefono, password, nomeUtente);
+        utenti.Add(utenteDaAggiungere);
+        return true;
+    }
 }
