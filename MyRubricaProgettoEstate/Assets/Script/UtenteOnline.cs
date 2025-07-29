a737be466ef84ad2439fd2e35393d1ff8aef2709
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-07-29 18:17:44 +0200

    Update UtenteOnline.cs
    scritto getter per attributo contenente l'utente

--- MyRubricaProgettoEstate/Assets/Script/UtenteOnline.cs
+++ MyRubricaProgettoEstate/Assets/Script/UtenteOnline.cs
@@ -3,4 +3,9 @@ using UnityEngine;
 public static class UtenteOnline
 {
     private static Utente utenteLoggato;
+
+    public static Utente GetUtenteLoggato()
+    {
+        return this.utenteLoggato;
+    }
 }
