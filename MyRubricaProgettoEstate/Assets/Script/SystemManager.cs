1533abe26b2602ddf945f345e2d6d98ec1220c1d
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-07-30 20:29:43 +0200

    Update SystemManager.cs
    aggiunti attributi che conteranno le varie schermate

--- MyRubricaProgettoEstate/Assets/Script/SystemManager.cs
+++ MyRubricaProgettoEstate/Assets/Script/SystemManager.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class SystemManager : MonoBehaviour
 {
-    
+    public GameObject panelloLogin;
+    public GameObject panelloRegistrazione;
+    public GameObject panelloProfilo;
+    public GameObject panelloRubrica;
+    public GameObject panelloAggiuntaContattoPrivato;
+    public GameObject panelloAggiuntaContattoLavorativo;
+    public GameObject panelloEliminazioneContatto;
+    public GameObject panelloRicercaContatto;
 }
