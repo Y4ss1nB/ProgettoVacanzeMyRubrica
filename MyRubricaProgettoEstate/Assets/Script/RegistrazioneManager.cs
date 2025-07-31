cd6b00444b26283446750556fcb2a79e8f3ed7a2
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-07-31 17:34:31 +0200

    creata classe RegistrazioneManager e scritti i suoi attributi

---
+++ MyRubricaProgettoEstate/Assets/Script/RegistrazioneManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using TMPro;
+public class RegistrazioneManager : MonoBehaviour
+{
+    public TMP_InputField inputNome;
+    public TMP_InputField inputNumeroDiTelefono;
+    public TMP_InputField inputPassword;
+    public GameObject scrittaDiErrore;
+}
