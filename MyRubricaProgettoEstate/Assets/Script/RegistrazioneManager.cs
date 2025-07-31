b0e2c6b7150efb744c5a088200e432725f73adf1
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-07-31 17:38:52 +0200

    Update RegistrazioneManager.cs
    scritti getter per gli attributi di RegistrazioneManager

--- MyRubricaProgettoEstate/Assets/Script/RegistrazioneManager.cs
+++ MyRubricaProgettoEstate/Assets/Script/RegistrazioneManager.cs
@@ -6,4 +6,17 @@ public class RegistrazioneManager : MonoBehaviour
     public TMP_InputField inputNumeroDiTelefono;
     public TMP_InputField inputPassword;
     public GameObject scrittaDiErrore;
+
+    public string GetInputNome()
+    {
+        return this.inputNome.text;
+    }
+    public string GetInputNumeroDiTelefono()
+    {
+        return this.inputNumeroDiTelefono.text;
+    }
+    public string GetInputPassword()
+    {
+        return this.inputPassword.text;
+    }
 }
