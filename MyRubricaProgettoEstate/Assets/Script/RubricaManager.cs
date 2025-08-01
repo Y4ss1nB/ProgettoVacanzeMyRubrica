0df0c64e375282927641fadf540050de5e4dd177
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-08-01 17:27:57 +0200

    Update RubricaManager.cs
    inserito metodo per mostrare i contatti nella schermata della rubrica

--- MyRubricaProgettoEstate/Assets/Script/RubricaManager.cs
+++ MyRubricaProgettoEstate/Assets/Script/RubricaManager.cs
@@ -1,9 +1,32 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
+
 
 public class RubricaManager : MonoBehaviour
 {
     public TMP_Text zonaInformazioniContatti;
     public Utente utente;
-    
+
+    public void MostraInformazioniContatti()
+    {
+        this.utente = UtenteOnline.GetUtenteLoggato();
+
+        if (this.utente == null)
+        {
+            return;
+        }
+
+        List<Contatto> contatti = this.utente.GetContatti();
+        foreach (Contatto contatto in contatti)
+        {
+            this.zonaInformazioniContatti.text += contatto.ToString();
+        }
+
+        if (contatti.Count == 0)
+        {
+            this.zonaInformazioniContatti.text = "Non ci sono ancora contatti...";
+        }
+    }
+
 }
