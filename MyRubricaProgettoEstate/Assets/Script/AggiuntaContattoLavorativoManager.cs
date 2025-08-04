a1cfc589d88f48debca96396f60968c12004e184
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-08-04 17:55:34 +0200

    Update AggiuntaContattoLavorativoManager.cs
    scritti attributi di AggiuntaContattoLavorativoManager

--- MyRubricaProgettoEstate/Assets/Script/AggiuntaContattoLavorativoManager.cs
+++ MyRubricaProgettoEstate/Assets/Script/AggiuntaContattoLavorativoManager.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
+using System;
+using TMPro;
 
 public class AggiuntaContattoLavorativoManager : MonoBehaviour
 {
-    
+    public TMP_InputField inputNomeContatto;
+    public TMP_InputField inputNumeroContatto;
+    public TMP_InputField inputEmailAziendaleContatto;
+    public TMP_InputField inputNomeAziendaContatto;
+    public GameObject scrittaDiErrore;
+
 }
