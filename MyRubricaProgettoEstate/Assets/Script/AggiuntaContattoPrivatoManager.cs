b64ea686189c436f77d6622b9271f2d044ac9ce8
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-08-04 16:27:17 +0200

    Update AggiuntaContattoPrivatoManager.cs
    scritti getter per attributi di AggiuntaContattoPrivatoManager

--- MyRubricaProgettoEstate/Assets/Script/AggiuntaContattoPrivatoManager.cs
+++ MyRubricaProgettoEstate/Assets/Script/AggiuntaContattoPrivatoManager.cs
@@ -8,5 +8,22 @@ public class AggiuntaContattoPrivatoManager : MonoBehaviour
     public TMP_InputField inputNumeroContatto;
     public TMP_InputField inputIndirizzoContatto;
     public TMP_InputField inputSoprannomeContatto;
-    public GameObject scrittaDiErrore; 
+    public GameObject scrittaDiErrore;
+
+    public string GetInputNomeContatto()
+    {
+        return this.inputNomeContatto.text;
+    }
+    public string GetInputNumeroContatto()
+    {
+        return this.inputNumeroContatto.text;
+    }
+    public string GetInputIndirizzoContatto()
+    {
+        return this.inputIndirizzoContatto.text;
+    }
+    public string GetInputSoprannomeContatto()
+    {
+        return this.inputSoprannomeContatto.text;
+    }
 }
