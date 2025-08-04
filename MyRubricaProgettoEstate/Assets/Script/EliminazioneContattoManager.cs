662db31b3172b58599b8e9336e44b99a06145fca
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-08-04 18:40:14 +0200

    Update EliminazioneContattoManager.cs
    scritti attributi di EiminazioneContattoManager

--- MyRubricaProgettoEstate/Assets/Script/EliminazioneContattoManager.cs
+++ MyRubricaProgettoEstate/Assets/Script/EliminazioneContattoManager.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
+using System;
+using TMPro;
 
 public class EliminazioneContattoManager : MonoBehaviour
 {
+    public TMP_InputField inputNumero;
+    public GameObject scrittaDiErrore;
+
     
 }
