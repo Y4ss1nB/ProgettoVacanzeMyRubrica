f3463bbad80b4b6bb2b24e139a5363eeaae07d77
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-08-04 19:08:09 +0200

    Update RicercaContattoManager.cs
    scritti attributi di RicercaContattoManager

--- MyRubricaProgettoEstate/Assets/Script/RicercaContattoManager.cs
+++ MyRubricaProgettoEstate/Assets/Script/RicercaContattoManager.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
+using System;
+using TMPro;
 
 public class RicercaContattoManager : MonoBehaviour
 {
-    
+    public TMP_InputField inputNome;
+    public GameObject scrittaDiErrore;
+    public TMP_Text InformazioniContatto;
 }
