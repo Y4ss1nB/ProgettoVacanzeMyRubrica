1c9ddcf02a92bb6d222f83cb2e66ddffe68cac93
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-08-04 16:17:03 +0200

    Update AggiuntaContattoPrivatoManager.cs
    aggiunti attributi ad AggiuntaContattoMnager

--- MyRubricaProgettoEstate/Assets/Script/AggiuntaContattoPrivatoManager.cs
+++ MyRubricaProgettoEstate/Assets/Script/AggiuntaContattoPrivatoManager.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
+using System;
+using TMPro;
 
 public class AggiuntaContattoPrivatoManager : MonoBehaviour
 {
-   
+    public TMP_InputField inputNomeContatto;
+    public TMP_InputField inputNumeroContatto;
+    public TMP_InputField inputIndirizzoContatto;
+    public TMP_InputField inputSoprannomeContatto;
+    public GameObject scrittaDiErrore; 
 }
