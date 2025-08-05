1d23bcb189e947ae95d4f202befbb580fcec05cd
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-08-05 18:56:12 +0200

    Update ComparatoreNumeroDiTelefonoDecrescente.cs
    implementato metodo Compare dell'interfaccia ICompare

--- MyRubricaProgettoEstate/Assets/Script/ComparatoreNumeroDiTelefonoDecrescente.cs
+++ MyRubricaProgettoEstate/Assets/Script/ComparatoreNumeroDiTelefonoDecrescente.cs
@@ -6,6 +6,11 @@ public class ComparatoreNumeroDiTelefonoDecrescente : IComparer<Contatto>
 {
     public ComparatoreNumeroDiTelefonoDecrescente()
     {
-        
+
+    }
+    
+    public int Compare(Contatto contattoUno, Contatto contattoDue)
+    {
+        return contattoDue.GetTelefono().CompareTo(contattoUno.GetTelefono());
     }
 }
