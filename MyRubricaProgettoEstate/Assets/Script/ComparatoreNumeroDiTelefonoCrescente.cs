088d136adc4f84618b0b7ff80b51cae562130e9e
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-08-05 18:44:35 +0200

    Update ComparatoreNumeroDiTelefonoCrescente.cs
    implementata interfaccia ICompare

--- MyRubricaProgettoEstate/Assets/Script/ComparatoreNumeroDiTelefonoCrescente.cs
+++ MyRubricaProgettoEstate/Assets/Script/ComparatoreNumeroDiTelefonoCrescente.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
+using System;
+using System.Collections.Generic;
 
-public class ComparatoreNumeroDiTelefonoCrescente
+public class ComparatoreNumeroDiTelefonoCrescente : IComparer<Contatto>
 {
     
 }
