aff5cae13a6207ed23ac17ba67bae2c2d0f0bc23
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-08-05 18:47:54 +0200

    Update ComparatoreNumeroDiTelefonoCrescente.cs
    scritto costruttore del comparatore e implementato metodo Compare dell'interfaccia ICompare

--- MyRubricaProgettoEstate/Assets/Script/ComparatoreNumeroDiTelefonoCrescente.cs
+++ MyRubricaProgettoEstate/Assets/Script/ComparatoreNumeroDiTelefonoCrescente.cs
@@ -4,5 +4,13 @@ using System.Collections.Generic;
 
 public class ComparatoreNumeroDiTelefonoCrescente : IComparer<Contatto>
 {
-    
+    public ComparatoreNumeroDiTelefonoCrescente()
+    {
+
+    }
+
+    public int Compare(Contatto contattoUno, Contatto contattoDue)
+    {
+        return contattoUno.GetTelefono().CompareTo(contattoDue.GetTelefono());
+    }
 }
